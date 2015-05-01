54e7e139fcc9d78fa573c83ee4a7b47ec5d76cf1
Alex Hall <Alexander.D.Hall13.civ@mail.mil>
2015-04-30 20:48:14 -0400

    Added comments to server

--- ClaxtonA3Server.java
+++ ClaxtonA3Server.java
@@ -4,20 +4,10 @@
  * and open the template in the editor.
  */
 
-
-/**
- *
- * @author Tommy
- */
 import java.util.*;
 import java.io.*;
 import java.net.*;
 
-
-/**
- *
- * @author Tommy
- */
 public class ClaxtonA3Server {
 
     /**
@@ -25,23 +15,37 @@ public class ClaxtonA3Server {
      */
     public static void main(String[] args) throws Exception {
         Scanner console = new Scanner(System.in);
-        System.out.print("Enter the name of a web server: ");
-        String w = console.nextLine();
-        System.out.println("Enter 1: HTTP Get Request to Web via as client");
-        System.out.println("Enter 2: Connect to Localhost to send HTTP get Request");
-        int selection = console.nextInt();
-        if(selection == 1) {
-            System.out.println("Sending HTTP as client now...");
-            httpRequest(w, 80, "GET " + 80 + " HTTP/1.0");
-            //from http://www.javaworld.com/article/2853780/java-app-dev/socket-programming-for-scalable-systems.html
-        }else if(selection == 2) {
-            System.out.println("Connecting Localhost Server now...");
-            System.out.println("Request viewable in server output Stream! Not here!");
-            httpRequest("0.0.0.0", 12337, w);
+        System.out.println("Option 1: ");
+        System.out.println("Option 2: ");
+        int option = console.nextInt();
+
+        //Create Table
+        //Read from WT.txt for starting node
+        //Populate tables
+
+        if(option == 1) {
+        //OPEN TCP
+        //Request DVR Data from client (reading frist number of messages)
+        //Update Routing Table
+        //Compute shortest node path.
+        //Print Router Tables
+        //Print Shortest Path
+            httpRequest("0.0.0.0", 33445);
         }
+        else if(option == 2) {
+        //Creat treads
+            //Open TCP
+                //Request DVR Data from client
+            //Update Routing Table
+                //Compute shortest node path.
+                //Print Each table
+        //Print Shortest Path
+        }
+
+        //Print total time taken
     }
     
-    public static void httpRequest(String w, int p, String message) throws Exception {
+    public static void httpRequest(String w, int p) throws Exception {
             Socket socket = new Socket(w, p); //connects
             PrintStream out = new PrintStream(socket.getOutputStream());
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
